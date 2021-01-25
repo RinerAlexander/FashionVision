ac6124db94dfa53379430aa6bc127b988316d68d
Alex Riner <rineralexander@gmail.com>
2021-01-24 19:04:50 -0600

    Fixed the model Finallygit add webpages/

--- webpages/app.py
+++ webpages/app.py
@@ -24,16 +24,8 @@ app = Flask(__name__)
 model = load_model ("EPOCH60ver2.h5")
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-    
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
 @app.route("/imageUpload", methods=["GET", "POST"])
-def imageUpload():
+def upload_picture():
 
     if request.method == "POST":
         print("first")
@@ -60,10 +52,8 @@ def imageUpload():
         
         # the model spits out a list of answers so we need to grab the 
         # first and only answer it gives
-        # answer=model.predict_classes(pixel_array)[0]
-        # answer=class_names[answer]
-        predictions = model.predict(pixel_array)
-        answer=class_names[np.argmax(predictions)]
+        answer=model.predict_classes(pixel_array)[0]
+        answer=class_names[answer]
 
         return render_template("imageUpload.html",picture=f"input/{file_name}",message=answer)
         
@@ -74,21 +64,20 @@ def imageUpload():
 def input(file):
     return send_from_directory("",f"{file}")
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
 
-# @app.route("/generic")
-# def generic():
-#     return render_template("generic.html")
-
 @app.route("/model")
-def model():
+def explanation():
     return render_template("model.html")
-
-# @app.route("/elements")
-# def elements():
-#     return render_template("elements.html")
 
 @app.route("/visual")
 def visual():
